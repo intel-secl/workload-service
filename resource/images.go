c1f0c72dae508e37f4cad93331d8ad59c3ede086
HarshithaGowda <harshitha.m.gowda@intel.com>
2019-08-21 10:45:16 -0700

    Add validation for image and flavor UUID formats with right error codes

--- resource/images.go
+++ resource/images.go
@@ -501,6 +501,18 @@ func createImage(db repository.WlsDatabase) endpointHandler {
 				StatusCode: http.StatusBadRequest,
 			}
 		}
+		// valid input format
+		if err := validation.ValidateUUIDv4(formBody.ID); err != nil {
+			log.Error("Invalid image UUID format")
+			return &endpointError{Message: err.Error(), StatusCode: http.StatusBadRequest}
+		}
+		for i, _ := range formBody.FlavorIDs {
+			if err := validation.ValidateUUIDv4(formBody.FlavorIDs[i]); err != nil {
+				log.Error("Invalid flavor UUID format")
+				return &endpointError{Message: err.Error(), StatusCode: http.StatusBadRequest}
+			}
+		}
+		
 		cLog := log.WithField("image", formBody)
 		if err := db.ImageRepository().Create(&formBody); err != nil {
 			switch err {
