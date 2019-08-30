152df8ee6648b5db71b806891a4be4edc17559fc
Rawat, Arvind <arvind.rawat@intel.com>
2019-08-30 03:29:18 -0700

    v1.0/copyright-headers: Added Intel copyright header.

--- version/version.go
+++ version/version.go
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2019 Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 package version
 
 // Version Number of the current build. Automatically filled in by linker
